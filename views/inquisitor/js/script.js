dcd3250344ac3696189fc4ab771d6f51b808d788
ksdigital <info@ksdigital.ru>
2023-01-09 06:52:24 +0300

    inquisitor updates 09.01

--- views/inquisitor/js/script.js
+++ views/inquisitor/js/script.js
@@ -3,7 +3,7 @@ function individuals() {
     document.getElementById("cards-3").setAttribute("style", "display: none");
     document.getElementById("cards-1").setAttribute("style", "display: flex");
 }
-    
+
 function legal() {
     document.getElementById("cards-1").setAttribute("style", "display: none");
     document.getElementById("cards-3").setAttribute("style", "display: none");
