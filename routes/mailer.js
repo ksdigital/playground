7ae060218bea092548226b9c0a401456c45af9fa
ksdigital <info@ksdigital.ru>
2023-07-13 18:18:21 +0300

    menu for tests vol.2

--- routes/mailer.js
+++ routes/mailer.js
@@ -4,5 +4,6 @@ const controller = require('../controllers/mailer')
 const router = express.Router()
 
 router.post('/', controller.send)
+router.post('/menu', controller.send)
 
 module.exports = router
